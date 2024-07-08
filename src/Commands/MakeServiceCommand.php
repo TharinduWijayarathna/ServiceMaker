2323ee28aaa076c046e4d2c83f8b40dba76d33b4
TharinduWijayarathna <wikum.dev@gmail.com>
2024-07-08 19:34:30 +0530

    Commands Updated

--- src/Commands/MakeServiceCommand.php
+++ src/Commands/MakeServiceCommand.php
@@ -7,30 +7,41 @@ use Illuminate\Support\Facades\File;
 
 class MakeServiceCommand extends Command
 {
-    protected $signature = 'make:service';
-
+    protected $signature = 'make:service {name?}';
     protected $description = 'Generate a service file';
 
     public function handle()
     {
-        $folder = $this->ask('Do you want to create a folder for the service? (yes/no)');
-        $folderName = '';
+        $name = $this->argument('name');
+        $folder = '';
+        $serviceName = '';
 
-        if ($folder === 'yes') {
-            $folderName = $this->ask('Enter the folder name for the service');
-            File::ensureDirectoryExists(app_path('Services/' . $folderName));
+        if ($name) {
+            // Split the name into folder and service name
+            $segments = explode('/', $name);
+            $serviceName = array_pop($segments);
+            $folder = implode('/', $segments);
+
+            if (!empty($folder)) {
+                File::ensureDirectoryExists(app_path('Services/' . $folder));
+            }
+        } else {
+            $folder = $this->ask('Enter the folder name for the service (leave empty for none)');
+            if (!empty($folder)) {
+                File::ensureDirectoryExists(app_path('Services/' . $folder));
+            }
+
+            $serviceName = $this->ask('Enter the service name');
         }
 
         $modelName = $this->ask('Enter the model name related to this service (e.g., Product)');
 
-        $serviceName = $this->ask('Enter the service name (e.g., ProductService)');
-
         $stub = file_get_contents(__DIR__ . '/stubs/service.stub');
-        $stub = str_replace('{{namespace}}', 'App\Services\\' . $folderName, $stub);
+        $stub = str_replace('{{namespace}}', 'App\Services\\' . $folder, $stub);
         $stub = str_replace('{{modelName}}', $modelName, $stub);
         $stub = str_replace('{{serviceName}}', $serviceName, $stub);
 
-        file_put_contents(app_path('Services/' . $folderName . '/' . $serviceName . '.php'), $stub);
+        file_put_contents(app_path('Services/' . $folder . '/' . $serviceName . '.php'), $stub);
 
         $this->info('Service created successfully!');
     }
